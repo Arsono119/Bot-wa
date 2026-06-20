16ccedc49225ee98da6b782e5192a27f03139c51
Arsono <Channelusmar@gmail.com>
2026-06-20 09:19:46 +0700

    update config dengan groq key

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    namaBot: 'Bot WA',
-    prefiks: '!',
     sesiLogin: 'sesi_login',
     pesanAktif: '\n🎉 BOT BERHASIL AKTIF!',
+    groqKey: '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',
 };
