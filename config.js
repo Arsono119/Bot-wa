f4a0fbc25b8163349af18a5f71afd1501c874b28
Arlius <151370854+Arsono119@users.noreply.github.com>
2026-06-22 02:54:06 +0700

    update config with keywords

--- config.js
+++ config.js
@@ -1,5 +1,16 @@
 module.exports = {
     sesiLogin: 'sesi_login',
-    pesanAktif: '\n🎉 BOT BERHASIL AKTIF!',
+    pesanAktif: '\nðŸŽ‰ BOT ANALISIS GRUP AKTIF!',
+
     groqKey: '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',
+
+    keywords: {
+        'urgent': ['628xxx@whatsapp.net'],
+        'penting': ['628xxx@whatsapp.net'],
+        'tolong': ['628yyy@whatsapp.net'],
+        'bantuan': ['628yyy@whatsapp.net'],
+        'error': ['628zzz@whatsapp.net'],
+        'server': ['628zzz@whatsapp.net'],
+        'review': ['628www@whatsapp.net'],
+    }
 };
