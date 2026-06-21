5fb999fe50c80441fb52147dc16d71345a66d1b3
Arlius <151370854+Arsono119@users.noreply.github.com>
2026-06-22 02:29:02 +0700

    hapus handler

--- handler/sapaan.js
+++
@@ -1,9 +0,0 @@
-const kataSapaan = ['halo', 'hi', 'hey', 'hai', 'hello', 'p'];
-
-async function sapaan(sock, id) {
-    await sock.sendMessage(id, {
-        text: `👋 Halo! Ada yang bisa dibantu?`
-    });
-}
-
-module.exports = { kataSapaan, sapaan };
